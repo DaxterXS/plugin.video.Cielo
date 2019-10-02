81f2e3cb9cf1f218b42683058c0cb9b4a7589273
Davide Bisso <33525392+DaxterXS@users.noreply.github.com>
2019-10-02 10:20:36 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Diretta SkyTG24 Kodi plugin
+
+Kodi unofficial plugin to stream Sky Tg24 from the official website.
+
+## Installation
+
+Download the contents of the repository as a zip file and then from the add-ons menu select "Install from zip file"
---
+++ addon.py
@@ -0,0 +1,20 @@
+import re
+import xbmcgui
+import requests
+
+url = 'http://videoplatform.sky.it/player/json/get_livestream_1.json'
+res = requests.get(url)
+
+try:
+	streaming_url = 'https://' + re.findall('"streaming_url":"https{0,1}://(.*?)"', res.text)[0]
+	thumb_url = 'https://' + re.findall('"img":"https{0,1}://(.*?)"', res.text)[1]
+	listitem = xbmcgui.ListItem('CIelo')
+	listitem.setInfo('video', {'Title': 'Diretta Cielo'})
+	listitem.setArt({ 'thumb': thumb_url})
+	xbmc.Player().play(streaming_url, listitem)
+
+except Exception as ex:
+	message = 'Eccezione: %s' % ex
+	listitem = xbmcgui.ListItem('Cielo - Errore')
+	listitem.setInfo('video', {'Title': 'Errore nell\' avvio del plugin Cielo'})
+	listitem.setInfo('video', { 'plot': message })
